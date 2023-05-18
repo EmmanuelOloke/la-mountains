669567176d6df55f036740bc7639e54b92a3e504
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-18 16:05:07 +0100

    mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
-import { Box, HStack, Image, Link } from '@chakra-ui/react';
+import {
+  Box,
+  HStack,
+  Image,
+  Link,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  VStack,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+
 import Logo from '../assets/Logo.png';
+import { useRef } from 'react';
 
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const navRef = useRef(null);
+
   return (
     <Box
       width="100vw"
@@ -35,6 +54,14 @@ const Navbar = () => {
               fontWeight="bold"
               color="#FFF"
               borderBottom="1.5px solid #FFF"
+              display={{
+                base: 'none',
+                sm: 'none',
+                md: 'none',
+                lg: 'flex',
+                xl: 'flex',
+                '2xl': 'flex',
+              }}
             >
               01. HISTORY
             </Link>
@@ -46,12 +73,86 @@ const Navbar = () => {
               fontWeight="bold"
               color="#FFF"
               borderBottom="1.5px solid #FFF"
+              display={{
+                base: 'none',
+                sm: 'none',
+                md: 'none',
+                lg: 'flex',
+                xl: 'flex',
+                '2xl': 'flex',
+              }}
             >
               02. TEAM
             </Link>
+
+            <Box
+              ref={navRef}
+              onClick={onOpen}
+              display={{
+                base: 'flex',
+                sm: 'flex',
+                md: 'flex',
+                lg: 'none',
+                xl: 'none',
+                '2xl': 'none',
+              }}
+            >
+              <HamburgerIcon boxSize={8} />
+            </Box>
           </HStack>
         </Box>
       </HStack>
+
+      <Drawer
+        isOpen={isOpen}
+        onClose={onClose}
+        placement="left"
+        initialFocusRef={navRef}
+        size="full"
+      >
+        <DrawerOverlay />
+
+        <DrawerContent>
+          <Box>
+            <DrawerHeader>
+              <Image src={Logo} alt="LA Mountain Logo" />
+            </DrawerHeader>
+
+            <DrawerCloseButton
+              fontSize="1.2rem"
+              marginTop={2.5}
+              color="#292F2E"
+              fontWeight="bold"
+            />
+          </Box>
+
+          <DrawerBody display="flex" justifyContent="center" alignItems="center">
+            <VStack gap="3rem">
+              <Link
+                _hover={{ textDecoration: 'none' }}
+                fontFamily="Lato"
+                fontStyle="italic"
+                fontWeight="bold"
+                color="#000"
+                borderBottom="1.5px solid #000"
+              >
+                01. HISTORY
+              </Link>
+
+              <Link
+                _hover={{ textDecoration: 'none' }}
+                fontFamily="Lato"
+                fontStyle="italic"
+                fontWeight="bold"
+                color="#000"
+                borderBottom="1.5px solid #000"
+              >
+                02. TEAM
+              </Link>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Box>
   );
 };
