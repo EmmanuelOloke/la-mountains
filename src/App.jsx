24d0940d7fdca9510b20afaeed03bc142606a39e
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-19 00:29:22 +0100

    edited custom css import link and added Climb and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { Box } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import { Box } from '@chakra-ui/react';
+import LAMountains from './components/LAMountains';
 import Histrory from './components/History';
+import Climb from './components/Climb';
+import Footer from './components/Footer';
 
-import './assets/splide-cutom.css';
+import './assets/splide-custom.css';
 
 function App() {
   return (
     <Box>
       <Navbar />
       <Hero />
+      <LAMountains />
       <Histrory />
+      <Climb />
+      <Footer />
     </Box>
   );
 }
