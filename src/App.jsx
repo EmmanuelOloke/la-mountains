37afbec9871575910ae96a9efcf89d9f8dca20c7
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-18 23:30:29 +0100

    imported custom css file for splidejs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import { Box } from '@chakra-ui/react';
+import Histrory from './components/History';
+
+import './assets/splide-cutom.css';
 
 function App() {
   return (
     <Box>
       <Navbar />
       <Hero />
+      <Histrory />
     </Box>
   );
 }
