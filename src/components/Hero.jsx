ca39580dbacf2f60f1b93b19924e20b5f56f6b58
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-18 16:05:42 +0100

    image mobile responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,19 @@
 import { Box, Image } from '@chakra-ui/react';
 import HeroImage from '../assets/hero-bg.png';
+import HeroImageMobile from '../assets/hero-bg-mobile.png';
 
 const Hero = () => {
   return (
-    <Box>
-      <Image src={HeroImage} alt="Hero background image" />
-    </Box>
+    <>
+      <Box>
+        <Image
+          display={{ base: 'flex', lg: 'none' }}
+          src={HeroImageMobile}
+          alt="Hero background image"
+        />
+        <Image display={{ base: 'none', lg: 'flex' }} src={HeroImage} alt="Hero background image" />
+      </Box>
+    </>
   );
 };
 
