cf658ce40470c1b3560a13197ae2365d4bca5030
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-19 00:27:54 +0100

    created LAMountains component

---
+++ src/components/LAMountains.jsx
@@ -0,0 +1,67 @@
+import { Box, HStack, Image, Link } from '@chakra-ui/react';
+
+import Logo2 from '../assets/Logo2.png';
+
+const LAMountains = () => {
+  return (
+    <Box width="100vw" display="flex" justifyContent="center">
+      <HStack
+        w={{ base: '100%', sm: '100%', md: '100%', lg: '60%', xl: '60%', '2xl': '60%' }}
+        padding={{
+          base: '1rem 1rem',
+          sm: '1rem 1rem',
+          md: '1rem 1rem',
+          lg: '1rem 0',
+          xl: '1rem 0',
+          '2xl': '1rem 0',
+        }}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Image src={Logo2} alt="LA Mountain" />
+
+        <HStack gap="3rem">
+          <Link
+            _hover={{ textDecoration: 'none' }}
+            fontFamily="Lato"
+            fontStyle="italic"
+            fontWeight="bold"
+            color="#414f6b"
+            borderBottom="1.5px solid #414f6b"
+            display={{
+              base: 'none',
+              sm: 'none',
+              md: 'none',
+              lg: 'flex',
+              xl: 'flex',
+              '2xl': 'flex',
+            }}
+          >
+            01. HISTORY
+          </Link>
+
+          <Link
+            _hover={{ textDecoration: 'none' }}
+            fontFamily="Lato"
+            fontStyle="italic"
+            fontWeight="bold"
+            color="#414f6b"
+            borderBottom="1.5px solid #414f6b"
+            display={{
+              base: 'none',
+              sm: 'none',
+              md: 'none',
+              lg: 'flex',
+              xl: 'flex',
+              '2xl': 'flex',
+            }}
+          >
+            02. TEAM
+          </Link>
+        </HStack>
+      </HStack>
+    </Box>
+  );
+};
+
+export default LAMountains;
