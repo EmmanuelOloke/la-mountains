e2f12d9ad4598a666ebb884de295a959cf25f7bc
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-19 11:53:51 +0100

    added id to component for smooth scrolling

--- src/components/Climb.jsx
+++ src/components/Climb.jsx
@@ -2,16 +2,8 @@ import { Box, HStack, Text } from '@chakra-ui/react';
 
 const Climb = () => {
   return (
-    <Box width="100vw" display="flex" justifyContent="center">
+    <Box id="climb" width="100vw" display="flex" justifyContent="center">
       <HStack
-        // padding={{
-        //   base: '1rem 1rem',
-        //   sm: '1rem 1rem',
-        //   md: '1rem 1rem',
-        //   lg: '1rem 0',
-        //   xl: '1rem 0',
-        //   '2xl': '1rem 0',
-        // }}
         w={{ base: '100%', sm: '100%', md: '100%', lg: '60%', xl: '58%', '2xl': '60%' }}
         gap="3rem"
       >
