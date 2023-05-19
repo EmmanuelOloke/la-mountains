f79016dabba7485d86ddacc28bffb983608fe6fe
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-20 00:28:09 +0100

    render MountainAccordion component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LAMountains from './components/LAMountains';
 import Histrory from './components/History';
 import Climb from './components/Climb';
 import MountainTabs from './components/MountainTabs';
+import MountainAccordion from './components/MountainAccordion';
 import Footer from './components/Footer';
 
 import './assets/splide-custom.css';
@@ -17,7 +18,12 @@ function App() {
       <LAMountains />
       <Histrory />
       <Climb />
-      <MountainTabs />
+      <Box display={{ base: 'none', md: 'block' }}>
+        <MountainTabs />
+      </Box>
+      <Box display={{ base: 'block', md: 'none' }}>
+        <MountainAccordion />
+      </Box>
       <Footer />
     </Box>
   );
